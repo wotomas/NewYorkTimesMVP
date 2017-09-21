c40c4b165767ec8dc7285d6daf47ae69747a5a70
Jihyok Kim <jkimab@connect.ust.hk>
2017-09-21 16:41:18 +0900

    feat: refactored some unused codes

--- app/src/main/java/info/kimjihyok/new_york_times_client/post/list/PostListActivity.java
+++ app/src/main/java/info/kimjihyok/new_york_times_client/post/list/PostListActivity.java
@@ -27,29 +27,14 @@ import info.kimjihyok.new_york_times_client.util.NavigationHelper;
  */
 public class PostListActivity extends BaseActivity implements PostListPresenter.View {
   private static final String TAG = "PostListActivity";
-  private static final boolean DEBUG = BuildConfig.DEBUG;
 
   private RecyclerView mRecyclerView;
   private RecyclerView.LayoutManager mLayoutManager;
   private PostListAdapter mPostListAdapter;
 
-
   @Inject DataController mDataController;
   @Inject NavigationHelper mNavigationHelper;
   @Inject PostListPresenter mPresenter;
-
-  private View.OnClickListener itemClickListenter = new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-      PostItem item = (PostItem) view.getTag();
-      if (item != null) {
-        onPostItemClick(item);
-      } else {
-        if (DEBUG) Log.d(TAG, "onClick(): view tag is null");
-      }
-
-    }
-  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -69,34 +54,25 @@ public class PostListActivity extends BaseActivity implements PostListPresenter.
 
   @Override
   protected void onScreenChangeToLandscape() {
-    if (DEBUG) Log.d(TAG, "onScreenChangeToLandscape(): running");
     mPostListAdapter.onScreenChangeToLandscape();
   }
 
   @Override
   protected void onScreenChangeToPortrait() {
-    if (DEBUG) Log.d(TAG, "onScreenChangeToPortrait(): running");
     mPostListAdapter.onScreenChangeToPortrait();
   }
 
   private void bindViews() {
     mRecyclerView = (RecyclerView) findViewById(R.id.container);
-    // depends on grid orientation change params for layout manager
-    // should reset layout manager on view rotate
     mLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
     mRecyclerView.setLayoutManager(mLayoutManager);
-    int displayMode = getResources().getConfiguration().orientation;
 
-    if (displayMode == Configuration.ORIENTATION_LANDSCAPE) {
-      mPostListAdapter = new PostListAdapter(new ArrayList<PostItem>(), false);
-    } else {
-      mPostListAdapter = new PostListAdapter(new ArrayList<PostItem>(), true);
-    }
-
-    mPostListAdapter.addOnClickListener(itemClickListenter);
+    mPostListAdapter = new PostListAdapter(new ArrayList<>(), getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE);
+    mPostListAdapter.addOnClickListener(view -> {
+      PostItem item = (PostItem) view.getTag();
+      if (item != null) onPostItemClick(item);
+    });
     mRecyclerView.setAdapter(mPostListAdapter);
-    //mPostListAdapter = new PostListAdapter(DebugUtil.getDummyData());
-
   }
 
   @Override
@@ -113,13 +89,11 @@ public class PostListActivity extends BaseActivity implements PostListPresenter.
 
   @Override
   public void onPostItemClick(PostItem postItem) {
-    if (DEBUG) Log.d(TAG, "onPostItemClick(): view: " + postItem.getUrl());
     mNavigationHelper.goToPostDetailPage(postItem.getUrl());
   }
 
   @Override
   public void onSubscribe(List<PostItem> list) {
-    if (DEBUG) Log.d(TAG, "onSubscribe(): list " + list.size());
     mPostListAdapter.addAll(list);
   }
 
